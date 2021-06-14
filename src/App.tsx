346aa8116ac7545ec85688774598e25403bcff10
taletski <kirill.taletski@gmail.com>
2021-06-14 09:17:07 +0300

    feat(): adds jitsu tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import "./App.css";
 import { Space, Button } from "antd";
 
+import { jitsuClient } from "@jitsu/sdk-js";
+
+const jitsu = jitsuClient({
+  key: "js.cehd6u5zn5ie6q1m6xfvis.cy7fgc3w4gstlctl6ndzp",
+});
+
+jitsu.set({ target: "WEB_APP" });
+
 function App() {
   return (
     <Space direction="vertical" align="center" className="main-container">
-      <Button type="primary">{"Emulate Click"}</Button>
-      <Button type="default" danger>
-        {"Emulate Error"}
+      <Button
+        type="primary"
+        onClick={(event) =>
+          jitsu.track("Primary button click", {
+            rawEvent: event.nativeEvent,
+          })
+        }
+      >
+        {"Primary Button"}
+      </Button>
+      <Button
+        type="default"
+        onClick={(event) =>
+          jitsu.track("Error button click", {
+            rawEvent: JSON.stringify(event.nativeEvent),
+          })
+        }
+        danger
+      >
+        {"Error Button"}
       </Button>
     </Space>
   );
